0b6bc2cf5f1498c45042eda5306867207fd0bfd0
gutaeho <sbpdt1062@gmail.com>
2023-12-16 18:03:47 +0900

    2480 개선

--- SwiftAlgorithm/SwiftAlgorithm/Stepper/main.swift
+++ SwiftAlgorithm/SwiftAlgorithm/Stepper/main.swift
@@ -7,16 +7,34 @@
 
 import Foundation
 
+// MARK: 첫 제출
+//let i = readLine()!.components(separatedBy: " ").map { Int($0)! }
+//
+//let s: Set = .init([i[0], i[1], i[2]])
+//
+//if s.count == 1 {
+//    print("\((s.min() ?? 0) * 1000 + 10000)")
+//} else if s.count == 2 {
+//    if i[0] == i[1] {
+//        print("\(i[0] * 100 + 1000)")
+//    } else if i[1] == i[2] {
+//        print("\(i[1] * 100 + 1000)")
+//    } else {
+//        print("\(i[2] * 100 + 1000)")
+//    }
+//} else {
+//    print("\((s.max() ?? 0) * 100)")
+//}
+
+// MARK: 개선
 let i = readLine()!.components(separatedBy: " ").map { Int($0)! }
 
-let numbers: Set = .init([i[0], i[1], i[2]])
-let sameNumber = numbers.min() ?? 1
-let maxNumber = numbers.max() ?? 1
-
-if numbers.count == 1 {
-    print("\(sameNumber * 1000 + 10000)")
-} else if numbers.count == 2 {
-    print("\(sameNumber * 100 + 1000)")
+if i[0] == i[1] && i[1] == i[2] {
+    print("\(i[0] * 1000 + 10000)")
+} else if i[0] == i[1] || i[1] == i[2] {
+    print("\(i[1] * 100 + 1000)")
+} else if i[0] == i[2] {
+    print("\(i[0] * 100 + 1000)")
 } else {
-    print("\(maxNumber * 100)")
+    print("\(i.max()! * 100)")
 }
