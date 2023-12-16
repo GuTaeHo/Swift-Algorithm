17138f66f0cb8de0e2d8bb9cfd7e9b97d9c331f2
gutaeho <sbpdt1062@gmail.com>
2023-12-16 17:18:48 +0900

    2480

--- SwiftAlgorithm/SwiftAlgorithm/Stepper/main.swift
+++ SwiftAlgorithm/SwiftAlgorithm/Stepper/main.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+let i = readLine()!.components(separatedBy: " ").map { Int($0)! }
+
+let numbers: Set = .init([i[0], i[1], i[2]])
+let sameNumber = numbers.min() ?? 1
+let maxNumber = numbers.max() ?? 1
+
+if numbers.count == 1 {
+    print("\(sameNumber * 1000 + 10000)")
+} else if numbers.count == 2 {
+    print("\(sameNumber * 100 + 1000)")
+} else {
+    print("\(maxNumber * 100)")
+}
