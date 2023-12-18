2debf27cad052a7e3cf47a2b69abba2ac0d956b6
gutaeho <sbpdt1062@gmail.com>
2023-12-19 06:47:25 +0900

    8393

--- SwiftAlgorithm/SwiftAlgorithm/Stepper/main.swift
+++ SwiftAlgorithm/SwiftAlgorithm/Stepper/main.swift
@@ -1,8 +1,22 @@
 //
-//  main.swift
+//  8393.swift
 //  SwiftAlgorithm
 //
 //  Created by 구태호 on 12/18/23.
 //
 
 import Foundation
+
+sum(Int(readLine()!)!)
+
+var total = 0
+var input = 0
+func sum(_ i: Int) {
+    if i > 1 {
+        total += i
+        input = i - 1
+        sum(input)
+    } else {
+        print("\(total + i)")
+    }
+}
