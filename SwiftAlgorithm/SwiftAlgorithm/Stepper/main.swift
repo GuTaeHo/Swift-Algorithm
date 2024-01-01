b98fc8ab54c22b51579893dcd86c3423d60627bf
gutaeho <sbpdt1062@gmail.com>
2024-01-01 23:52:38 +0900

    11021

--- SwiftAlgorithm/SwiftAlgorithm/Stepper/main.swift
+++ SwiftAlgorithm/SwiftAlgorithm/Stepper/main.swift
@@ -1,51 +1,14 @@
 //
-//  main.swift
+//  11021.swift
 //  SwiftAlgorithm
 //
-//  Created by 구태호 on 12/20/23.
+//  Created by 구태호 on 1/1/24.
 //
 
 import Foundation
 
 let i = Int(readLine()!)!
-let fio = FileIO()
-
-var result = ""
-
 for j in 0..<i {
-    result += "\(fio.readInt() + fio.readInt())\n"
-}
-
-print(result)
-
-
-final class FileIO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
-    
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-    }
-    
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-        
-        return buffer[index]
-    }
-    
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-        
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-        
-        return sum * (isPositive ? 1:-1)
-    }
+    let k = readLine()!.components(separatedBy: " ").map { Int($0)! }
+    print("Case #\(j + 1): \(k[0] + k[1])")
 }
